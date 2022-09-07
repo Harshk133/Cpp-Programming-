53f2bbd3cf3499a0c325ca08ab13e55f6302a621
Harsh <harshmkale.2004@gmail.com>
2022-09-07 23:18:20 +0530

    Added Program1.cpp!

---
+++ Program1.cpp
@@ -0,0 +1,14 @@
+//Write a C++ Program to write Jay Ganesh On Screen.
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+cout<<"Jay Ganesh!\n";
+cout<<"Jay Ganesh!\n";
+cout<<"Jay Ganesh!\n";
+cout<<"Jay Ganesh!\n";
+
+getch();
+return 0;
+}
