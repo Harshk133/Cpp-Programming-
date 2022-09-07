92f8ec875316c2b303c46a3c7e51a7e03a35aca1
Harsh <harshmkale.2004@gmail.com>
2022-09-07 23:28:26 +0530

    Added Program3.cpp!

---
+++ program3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+int x, y, z;
+cout<<"Enter A First Number:\n";
+cin>>x;
+
+cout<<"Enter A Second Number:\n";
+cin>>y;
+
+z=x+y;
+
+cout<<"The Addition is "<<z;
+
+getch();
+return 0;
+}
