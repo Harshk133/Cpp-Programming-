fc25e381cdc36d66feac16f6e702e3778739855d
Harsh <harshmkale.2004@gmail.com>
2022-09-07 23:22:37 +0530

    Added Program2.cpp!

---
+++ program2.cpp
@@ -0,0 +1,18 @@
+//Write C++ Program to Accept Number and Display it's Square.
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+int x, y;
+
+cout<<"Enter A Number:\n";
+cin>>x;
+
+y=x*x;
+
+cout<<"The Square of is: "<<y;
+
+getch();
+return 0;
+}
