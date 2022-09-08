4468de4a87da7cc8a3f354327b8c2f6ef52ee742
Harsh <harshmkale.2004@gmail.com>
2022-09-08 08:09:04 +0530

    Added Program4.cpp!

--- program3.cpp
+++ program3.cpp
@@ -1,3 +1,4 @@
+//Program to add two numbers:)
 #include<iostream>
 #include<conio.h>
 using namespace std;
---
+++ program4.cpp
@@ -0,0 +1,20 @@
+//Substraction C++ Program
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+int x, y, z;
+cout<<"Enter the First Number:\n";
+cin>>x;
+
+cout<<"Enter the Second Number:\n";
+cin>>y;
+
+z=x-y;
+
+cout<<"The Substraction is: "<<z;
+
+getch();
+return 0;
+}
