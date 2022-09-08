a67c99fe402b9659e00392e9d5505463cde18088
Harsh <harshmkale.2004@gmail.com>
2022-09-08 08:23:27 +0530

    Added Program7.cpp!

---
+++ program7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+int n;
+
+cout<<"Enter the Number Want to print \'Jay Ganesh\' On the Screen:\n";
+cin>>n;
+
+for(int i=1; i<=n; i++){
+cout<<"Jay Ganesh!\n";
+}
+
+getch();
+return 0;
+}
