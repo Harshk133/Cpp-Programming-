fac883d54040a631324213fed7f6585faaf56a67
Harsh <harshmkale.2004@gmail.com>
2022-09-08 08:18:30 +0530

    Added Program6.cpp!

---
+++ program6.cpp
@@ -0,0 +1,21 @@
+//Program C++ For Division.
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+int x, y, z;
+
+cout<<"Enter the First Number:\n";
+cin>>x;
+
+cout<<"Enter the Second Number:\n";
+cin>>y;
+
+z=x/y;
+
+cout<<"The Division is: "<<z;
+
+getch();
+return 0;
+}
